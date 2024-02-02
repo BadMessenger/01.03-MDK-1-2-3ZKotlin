6df8183cbcbb8baf440e1c04a59098ac37403766
BadMessenger <157877951+BadMessenger@users.noreply.github.com>
2024-02-02 09:09:08 +0300

    Add files via upload
    Тута 3 задания в проекте

---
+++ 3 PZ/src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    //задание 1
+    //val amount= 100000.0
+    //val p= 0.75;
+    //val otv: Double;
+    //if (amount * p / 100 >= 35)
+    //    otv = amount - (amount * p / 100);
+    //else
+    //    otv = amount - 35;
+    //print(otv)
+
+    //задание 2
+    //val scanner = Scanner(System.`in`)
+    //println("Сколько лайков?")
+    //val likes = scanner.nextInt()
+    //if (likes % 2 == 1)
+    //    println("Понравилось " + likes + " человеку")
+    //if (likes % 2 == 0)
+    //    println("Понравилось " + likes + " людям")
+
+    //задание 3
+    //val scanner = Scanner(System.`in`)
+    //println("Какая сумма покупки?")
+    //val sum = scanner.nextInt()
+    //if ((0 < sum) && (sum < 1000))
+    //    println("Скидка не предоставляется")
+    //if ((1001 < sum) && (sum < 10_000))
+    //    println("Скидка составляет 100 рублей")
+    //if (10_001 < sum)
+    //    println("Скидка составляет 5% от суммы")
+}
